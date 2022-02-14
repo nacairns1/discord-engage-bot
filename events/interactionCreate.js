3f8ead4046c0c9ce9961ad0ae0a529bf35d9296e
nacairns1 <94420090+nacairns1@users.noreply.github.com>
2022-02-13 19:33:39 -0500

    button-update

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,8 +2,9 @@
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
-        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+
         if (!interaction.isCommand()) return;
+        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered a slash command interaction.`);
         const command = interaction.client.commands.get(interaction.commandName);
         if (!command) return;
         try {
